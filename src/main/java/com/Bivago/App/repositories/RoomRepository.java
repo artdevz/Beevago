d9ccd7a69f04b74b45ea37dc487e7fda4d737daa
artdevz <prodevarthurdantas@gmail.com>
2024-08-15 14:40:53 -0300

    Exceptions removidas e Query iniciada

--- src/main/java/com/Bivago/App/repositories/RoomRepository.java
+++ src/main/java/com/Bivago/App/repositories/RoomRepository.java
@@ -1,17 +1,20 @@
 package com.Bivago.App.repositories;
 
 //import java.util.List;
-//import java.util.UUID;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.Bivago.App.models.RoomModel;
 
 @Repository
 public interface RoomRepository extends JpaRepository<RoomModel, Long> {
-    
+    // BUG no UUID do Hotel
+    @Query(nativeQuery = true, value = "SELECT * FROM room WHERE HOTELUUID = :hotelUuid AND ROOMNUMBER = :roomNumber")
+    public RoomModel findByRoomNumber(UUID hoteluuid, int roomNumber);
+
     // @Query(nativeQuery = true, value = "")
     // public List<RoomModel> findById();
 
